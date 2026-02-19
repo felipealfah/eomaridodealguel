24ee6530d959c07cd058810a618da327a4e3d31b
felipealfah <felipealfah@gmail.com>
2026-02-19 10:38:07 -0300

    Config: Update site URL to custom domain https://eomaridodealuguel.com.br for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eomaridodealguel.pages.dev',
+  site: 'https://eomaridodealuguel.com.br',
 
   integrations: [
     react(),
